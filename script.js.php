2b9036b756fc61fb7582742ab8eb9cf097634681
Mike Martin <mtmgoliath@gmail.com>
2021-02-23 10:58:56 +0000

    made object array

---
+++ script.js.php
@@ -0,0 +1,68 @@
+<?php
+
+let cardObjectsArray = [
+    {
+        backOfCard : 'images/cardBack.jpg',
+        frontOfCard : 'images/pinkHamster.jpg'
+    },
+    {
+        backOfCard : 'images/cardBack.jpg',
+        frontOfCard : 'images/pinkHamster.jpg'
+    },
+    {
+        backOfCard : 'images/cardBack.jpg',
+        frontOfCard : 'images/wheelHamster.jpg'
+    },
+    {
+        backOfCard : 'images/cardBack.jpg',
+        frontOfCard : 'images/wheelHamster.jpg'
+    },
+    {
+        backOfCard : 'images/cardBack.jpg',
+        frontOfCard : 'images/yellowHamster.jpg'
+    },
+    {
+        backOfCard : 'images/cardBack.jpg',
+        frontOfCard : 'images/yellowHamster.jpg'
+    },
+    {
+        backOfCard : 'images/cardBack.jpg',
+        frontOfCard : 'images/coralHamster.jpg'
+    },
+    {
+        backOfCard : 'images/cardBack.jpg',
+        frontOfCard : 'images/coralHamster.jpg'
+    },
+    {
+        backOfCard : 'images/cardBack.jpg',
+        frontOfCard : 'images/greenHamster.jpg'
+    },
+    {
+        backOfCard : 'images/cardBack.jpg',
+        frontOfCard : 'images/greenHamster.jpg'
+    },
+    {
+        backOfCard : 'images/cardBack.jpg',
+        frontOfCard : 'images/tealHamster.jpg'
+    },
+    {
+        backOfCard : 'images/cardBack.jpg',
+        frontOfCard : 'images/tealHamster.jpg'
+    },
+    {
+        backOfCard : 'images/cardBack.jpg',
+        frontOfCard : 'images/handHamster.jpg'
+    },
+    {
+        backOfCard : 'images/cardBack.jpg',
+        frontOfCard : 'images/handHamster.jpg'
+    },
+    {
+        backOfCard : 'images/cardBack.jpg',
+        frontOfCard : 'images/smokingHamster.jpg'
+    },
+    {
+        backOfCard : 'images/cardBack.jpg',
+        frontOfCard : 'images/smokingHamster.jpg'
+    }
+]
